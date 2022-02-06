cc20142b06ce9d64e034991ad3fcd875a80474cb
vahehak93 <vahehak@gmail.com>
2022-02-06 21:31:11 +0400

    feat: added promise.js

---
+++ promise.js
@@ -0,0 +1,19 @@
+function func() {
+  return new Promise((res, rej) => {
+    setTimeout(function () {
+      rej(new Error("ERORRRRR"));
+    }, 1000);
+  });
+}
+
+const p = func();
+
+p.then((result) => {
+  alert(result);
+})
+  .then(() => {
+    return 1;
+  })
+  .catch((error) => {
+    alert(error);
+  });
